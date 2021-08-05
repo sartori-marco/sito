ddfc2e1dd498ff42d36beef7e94606c89b5bff39
sartori-marco <marcosartori98@yahoo.com>
2021-08-05 13:00:11 +0200

    modifica larghezza immagini

--- js/login.js
+++ js/login.js
@@ -14,7 +14,7 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "admin" && password == "administrator"){
-alert ("Login successfully");
+// alert ("Login successfully");
 window.location = "ibanner.html"; // Redirecting to other page.
 return false;
 }
