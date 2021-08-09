4a41c6fe18024b4eca9f472aaf87a6ce69b2527a
sartori-marco <marcosartori98@yahoo.com>
2021-08-09 11:12:59 +0200

    Add files via upload

--- js/calculator.js
+++ js/calculator.js
@@ -41,11 +41,12 @@ function calculatorLength(){
     var tc = document.getElementById("tank_capacity").value;
     var stints = risSafe / tc + +1;
     // RISULTATO STINTS
-    document.getElementById("stints").innerHTML = 'Stints: ' + stints.toFixed(0);
+    document.getElementById("stints").innerHTML = 'Stints: ' + Math.round(stints);
 
     // CALCOLO ULTIMO STINT
     var lastStint = risSafe - (tc * stints - 1);
-    document.getElementById("last_stint").innerHTML = 'Last stint: ' + lastStint.toFixed(0);
+    // document.getElementById("last_stint").innerHTML = 'Last stint: ' + lastStint.toFixed(0);
+    document.getElementById("last_stint").innerHTML = 'Last stint: ' + Math.round(lastStint);
 
     // CALCOLO DURATA GARA
     var durata = rm / time;
